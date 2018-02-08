30b2ce480c5d73e7e610596087ae2c18ce05f808
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-02-08 10:22:27 -0800

    solves range

--- src/recursion.js
+++ src/recursion.js
@@ -76,7 +76,17 @@ var sumBelow = function(n) {
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
-var range = function(x, y) {};
+var range = function(x, y) {
+  if (x === y || x + 1 === y || x - 1 === y) {
+    return [];
+  }
+
+  if (x < y) {
+    return [x + 1].concat(range(x + 1, y));
+  } else {
+    return [x - 1].concat(range(x - 1, y));
+  }
+};
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
