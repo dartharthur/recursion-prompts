77b943a2fabc5e27dc1b10275e65d4dada97b843
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-02-09 14:51:20 -0800

    solves compareStrings

--- src/recursion.js
+++ src/recursion.js
@@ -208,7 +208,15 @@ var gcd = function(x, y) {};
 // both are identical.
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
-var compareStr = function(str1, str2) {};
+var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  } else if (str1[0] !== str2[0]) {
+    return false;
+  }
+
+  return compareStr(str1.slice(1), str2.slice(1));
+};
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
