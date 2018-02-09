6dbe3c669d3768ed4e6c51a695f3e2633156ed22
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-02-09 15:32:36 -0800

    solves fizzBuzz

--- src/recursion.js
+++ src/recursion.js
@@ -253,7 +253,21 @@ var buildList = function(value, length) {
 // For multiples of five, output 'Buzz' instead of the number.
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
-var fizzBuzz = function(n) {};
+var fizzBuzz = function(n) {
+  if (n === 0) {
+    return [];
+  }
+
+  if (n % 5 === 0 && n % 3 === 0) {
+    return fizzBuzz(n - 1).concat(['FizzBuzz']);
+  } else if (n % 5 === 0) {
+    return fizzBuzz(n - 1).concat(['Buzz']);
+  } else if (n % 3 === 0) {
+    return fizzBuzz(n - 1).concat(['Fizz']);
+  } else {
+    return fizzBuzz(n - 1).concat([`${n}`]);
+  }
+};
 
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
