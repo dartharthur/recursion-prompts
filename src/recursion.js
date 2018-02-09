c7994bbb76e5c92cf412c543f9ef3bfc6f8d5b98
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-02-09 15:16:38 -0800

    solves createArray

--- src/recursion.js
+++ src/recursion.js
@@ -220,7 +220,13 @@ var compareStr = function(str1, str2) {
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
-var createArray = function(str) {};
+var createArray = function(str) {
+  if (str.length === 0) {
+    return [];
+  }
+
+  return [str[0]].concat(createArray(str.slice(1)));
+};
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {};
