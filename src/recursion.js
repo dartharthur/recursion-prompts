8846e8c28a23838e85def5ac1ba115865f452b67
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-02-09 15:20:09 -0800

    solves buildList

--- src/recursion.js
+++ src/recursion.js
@@ -240,7 +240,13 @@ var reverseArr = function(array) {
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
-var buildList = function(value, length) {};
+var buildList = function(value, length) {
+  if (length === 0) {
+    return [];
+  }
+
+  return [value].concat(buildList(value, length - 1));
+};
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
 // For multiples of three, output 'Fizz' instead of the number.
