b47fa33cd7c6d91a72f583f7951c911233f6b750
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-02-09 12:39:00 -0800

    pauses work on modulo

--- src/recursion.js
+++ src/recursion.js
@@ -155,19 +155,24 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (y < 0 && x < 0) {
+    y = -y;
+    x = -x;
+  } else if (y < 0) {
+    y = -y;
+  } else if (x < 0) {
+    x = -x;
+  }
+
   if (x === 0 && y === 0) {
     return NaN;
   } else if (x - y === y) {
     return 0;
-  } else if (y > 0 && x < y) {
+  } else if (x < y) {
     return x;
   }
 
-  if (y < 0) {
-    return modulo(x - -y, -y);
-  } else {
-    return modulo(x - y, y);
-  }
+  return modulo(x - y, y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
